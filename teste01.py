cf57940c64bebad9dfaa5f2ed04c907a285f5f19
Stey Silva <steylon123@hotmail.com>
2024-03-25 23:28:33 -0300

    fiz meu primeiro código em python

---
+++ teste01.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+city = input('Where are you from? ')
+print(f'Your name is {name}, you are {age} years old and live in {city}')
+
